db12753b732819c2d55f536ce9863aabb577ab62
masa6372 <songpuzheng@gmail.com>
2020-08-10 07:31:03 +0900

    add solution using concatMap

--- 7.hs
+++ 7.hs
@@ -4,3 +4,7 @@ flatten :: (NestedList a) -> [a]
 flatten (Elem a) = [a]
 flatten (List (x:xs)) = flatten (x) ++ flatten (List xs)
 flatten (List []) = []
+
+flatten' :: (NestedList a) -> [a]
+flatten' (Elem a) = [a]
+flatten' (List b) = concatMap flatten' b
