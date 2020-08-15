4c56758b3b55fb4251e1df0a836e7f869d920720
masa6372 <songpuzheng@gmail.com>
2020-08-15 17:28:52 +0900

    add a solution of problem 12

---
+++ 12.hs
@@ -0,0 +1,7 @@
+data ListItem a = Single a | Multiple Int a
+    deriving (Show)
+decodeModified :: [ListItem a] -> [a]
+decodeModified = concatMap decodeHelper
+    where
+        decodeHelper (Single x) = [x]
+        decodeHelper (Multiple n x) = replicate n x
